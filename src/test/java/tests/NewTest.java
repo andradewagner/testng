8780ebd923502dd3c946e092d5be482b80d0adeb
Wagner Andrade <andradewagner@hotmail.com>
2019-01-13 01:18:49 -0200

    driver dentro do projeto

--- src/test/java/tests/NewTest.java
+++ src/test/java/tests/NewTest.java
@@ -25,7 +25,7 @@ public class NewTest {
 
     @BeforeClass
     public void beforeClass() {
-        System.setProperty("webdriver.chrome.driver", "/home/wagner/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("disable-infobars");
